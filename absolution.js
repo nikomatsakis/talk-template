50a1c352a8e456a68cd48a5770a4a57f4d3231fd
Niko Matsakis <niko@alum.mit.edu>
2024-09-01 18:15:47 -0400

    Update absolution.js
    common additions

--- absolution.js
+++ absolution.js
@@ -3,6 +3,10 @@ function absolution() {
         console.log(`found element ${JSON.stringify(e.classList)}`);
         var top = NaN;
         var left = NaN;
+        var width = undefined;
+        var rotate = NaN;
+        var fontSize = NaN;
+
         for (const c of e.classList) {
             if (c.startsWith("top")) {
                 top = Number(c.substring(3));
@@ -10,6 +14,18 @@ function absolution() {
 
             if (c.startsWith("left")) {
                 left = Number(c.substring(4));
+            }
+
+            if (c.startsWith("width")) {
+                width = c.substring(5);
+            }
+
+            if (c.startsWith("rotate")) {
+                rotate = c.substring(6);
+            }
+
+            if (c.startsWith("fontSize")) {
+                fontSize = c.substring(8);
             }
         }
 
@@ -24,5 +40,32 @@ function absolution() {
             console.log(`found left ${left}`);
             e.style.left = `${left}px`;
         }
+
+        if (width) {
+            console.log(`found width ${width}`);
+            e.style.width = `${width}`;
+        }
+
+        if (rotate == rotate) {
+            console.log(`found rotate ${rotate}`);
+            e.style.transform = `rotate(${rotate}deg)`;
+        }
+
+        if (fontSize == fontSize) {
+            console.log(`found fontSize ${fontSize}`);
+            e.style.fontSize = `${fontSize}`;
+        }
+
+        e.onclick = (event) => {
+            let left = e.style.left;
+            let top = e.style.top;
+            let combined = `${left},${top}`;
+            let newvalue = window.prompt("Set left,top", combined);
+            if (newvalue) {
+                let parts = newvalue.split(",");
+                e.style.left = parts[0];
+                e.style.top = parts[1];
+            }
+        };
     }
 }
